90019067736f6d937cf261b0e01e790107b93f3f
Franz <franze2019@gmail.com>
2022-07-03 22:46:47 -0700

    modified display-textbox css

--- front-end/src/app/components/display-textbox/display-textbox.component.ts
+++ front-end/src/app/components/display-textbox/display-textbox.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./display-textbox.component.css']
 })
 export class DisplayTextboxComponent implements OnInit {
-  randomWords:string = "the be of and a to in he have it that for they i with as not on you do but from or which one would all will there say who make when can more if"
+  randomWords:string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis fringilla viverra lacinia. Praesent sed mauris vitae neque ultrices malesuada. Praesent porttitor, dui et fringilla tempus, libero tortor luctus mi, eget auctor dolor neque et ante. In ac dignissim ipsum. Quisque lacus dolor, bibendum ut aliquam a, congue a nulla. Etiam auctor vel magna et consequat. Vivamus lacinia lacus in metus."
+  // the be of and a to in he have it that for they i with as not on you do but from or which one would all will there say who make when can more if
   constructor() { }
 
   ngOnInit(): void {
