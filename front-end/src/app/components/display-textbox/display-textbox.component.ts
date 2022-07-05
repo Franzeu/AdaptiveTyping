87fa3a596fd4f9f9af0deba5cd98297671be3909
Franz <franze2019@gmail.com>
2022-07-05 11:54:10 -0700

    Modified CSS for header, typing text-box, and display-textbox

--- front-end/src/app/components/display-textbox/display-textbox.component.ts
+++ front-end/src/app/components/display-textbox/display-textbox.component.ts
@@ -6,11 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./display-textbox.component.css']
 })
 export class DisplayTextboxComponent implements OnInit {
-  randomWords:string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis fringilla viverra lacinia. Praesent sed mauris vitae neque ultrices malesuada. Praesent porttitor, dui et fringilla tempus, libero tortor luctus mi, eget auctor dolor neque et ante. In ac dignissim ipsum. Quisque lacus dolor, bibendum ut aliquam a, congue a nulla. Etiam auctor vel magna et consequat. Vivamus lacinia lacus in metus."
-  // the be of and a to in he have it that for they i with as not on you do but from or which one would all will there say who make when can more if
-  constructor() { }
+  randomWords:string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus rhoncus tortor ullamcorper, aliquet tortor at, dictum ante. Cras interdum orci at nisl pellentesque tincidunt. Ut ornare non ligula ut porttitor. Etiam quis nulla id tellus pellentesque vulputate. Fusce vitae eros lorem. Nullam non rhoncus lectus, sit amet blandit dui. Donec sit amet justo aliquet, ultricies tortor at, dapibus tellus. Quisque."
+  constructor( ) { }
 
   ngOnInit(): void {
+
   }
   
 }
