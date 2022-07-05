1bf1a83a0bf4205140f9255128b1e0e95370cd82
Brian Cornelius <brian@Brians-MacBook-Pro.local>
2022-07-05 00:54:54 -0700

    updated routes

--- back-end/routes.js
+++ back-end/routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const {test_res,getrandomtext} = require('./controllers/api_controller');
+const {test_res,getrandomtext, populate_words} = require('./controllers/api_controller');
 
 const router = express.Router();
 
 router.get('/test', test_res);
 router.get('/randomtext', getrandomtext);
+router.put('/populate', populate_words);
 module.exports = {
 
     routes: router
