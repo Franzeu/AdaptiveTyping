98d648388e3de72c3af32fd5243240981d3b26fe
Hang Xian <hsxian@ucsc.edu>
2022-07-08 23:49:19 -0700

    implemented display of wpm and accuracy when test is done

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { TitleStrategy } from '@angular/router';
+import { ChangeDetectorRef, AfterContentChecked } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,15 @@ import { TitleStrategy } from '@angular/router';
 export class AppComponent {
   @Input() target!: String;
   @Input() signalTimer!: boolean;
+  @Input() done: boolean = false;
+  @Input() userInput!: String;
+  @Input() time!: number;
+
+  constructor(private cdref: ChangeDetectorRef) {}
+
+  ngAfterContentChecked() {
+    this.cdref.detectChanges();
+  }
 
   start() {
     console.log("start");
@@ -16,10 +26,19 @@ export class AppComponent {
 
   setTarget(target: String) {
     this.target = target;
-    
   }
 
   sendSignalTimer(signalTimer: boolean) {
       this.signalTimer = signalTimer;
+  }
+
+  typingDone(userInput: String) {
+    this.done = true;
+    this.userInput = userInput;
+  }
+
+  timerDone(time: number) {
+    this.done = true;
+    this.time = time;
   }
 }
--- front-end/src/app/components/result/result.component.ts
+++ front-end/src/app/components/result/result.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.css']
 })
-export class ResultComponent implements OnInit {
+export class ResultComponent implements OnInit, DoCheck {
+  @Input() testDone: boolean = false;
+  @Input() timeDone: boolean = false;
+  @Input() userInput!: String;
+  @Input() target!: String;
+  @Input() time!: number;
+  wpm!: string;
+  accuracy!: string;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngDoCheck(): void {
+    console.log(this.userInput);
+    console.log(this.time);
+    this.wpm = ((this.userInput.length / 5) / ((60 - this.time) / 60)).toFixed(2);
+    let correct = 0;
+    for (let i = 0; i < this.userInput.length; i++) {
+      if (this.userInput[i] == this.target[i]) {
+        correct += 1;
+      }
+    }
+
+    this.accuracy = (correct / this.target.length * 100).toFixed(2);
+  }
 }
