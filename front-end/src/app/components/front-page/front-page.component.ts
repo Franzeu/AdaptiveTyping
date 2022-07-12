d155e88e7af062476017e57ca803732ceb6b4a5d
cwillia5 <cwillia5@ucsc.edu>
2022-07-12 11:46:29 -0700

    No longer displays user info on typing page

--- front-end/src/app/components/front-page/front-page.component.ts
+++ front-end/src/app/components/front-page/front-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { TitleStrategy } from '@angular/router';
 import { ChangeDetectorRef, AfterContentChecked } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-root',
