2e25cf7471263584ede10c643e0e04717ee3821f
cwillia5 <cwillia5@ucsc.edu>
2022-07-15 17:55:01 -0700

    adjusted routes so non user id routes call getrandomtext

--- back-end/controllers/api_controller.js
+++ back-end/controllers/api_controller.js
@@ -62,7 +62,7 @@ const test_res = async  (req, res, next) => {
 
 
 
-const getrandomtext = async (req,res,next) =>{
+const getrandomadapttext = async (req,res,next) =>{
     const numwords = 30; //number of words that the textbox displays
 
     try{
@@ -164,6 +164,44 @@ const getrandomtext = async (req,res,next) =>{
     }
 }
 
+const getrandomtext = async (req,res,next) =>{
+    const numwords = 50; //number of words that the textbox displays
+    const numletters = 4; //number of letter array fields in document
+
+    try{
+        const text = await firestore.collection('Words'); //word lists are stored in Words collection 
+        const data = await text.get();
+        let textArray = [];
+
+        if(data.empty){
+            res.status(500).send('firestore Words collection is empty');
+        }
+        else{
+            data.forEach(doc =>{
+
+                const keys = Object.keys(doc.data());
+                
+
+                for (let i = 0; i < numwords; i++) {
+
+                    let arr = doc.data()[keys[getRandomInt(keys.length)]]
+                    textArray.push(arr[getRandomInt(arr.length)]);
+                }
+                  
+                res.json({english: textArray});
+            })
+        }
+    }
+
+    catch(error){
+            res.status(400).send(error.message);
+            //res.send('Doc Failure');
+
+    }
+}
+
+
+
 // function to populate firestore words collection with data from local file, still being implemented
 const populate_words = async (req, res, next) => {
     console.log('populate_words called');
@@ -250,6 +288,7 @@ const get_usr_data = async(req, res, next) => {
 module.exports = {
 
     test_res,
+    getrandomadapttext,
     getrandomtext,
     populate_words,
     store_usr_data,
